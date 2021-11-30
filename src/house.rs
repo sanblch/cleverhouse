164b455aaf220ae788686bbaef4e5c7c5744be5c
Alexander Krutikov <s4nblch@gmail.com>
2021-11-30 23:21:24 +0300

    upgrade program by adding Option, Result

--- src/house.rs
+++ src/house.rs
@@ -13,15 +13,15 @@ impl House {
         }
     }
 
-    pub fn add_room(&self, _room: Room) -> Result<(), String> {
+    pub fn add_room(&self, _room: Room) -> Result<(), Error> {
         todo!()
     }
 
-    pub fn get_room(&self, _name: String) -> Result<Room, String> {
+    pub fn get_room(&self, _name: String) -> Option<Room> {
         todo!()
     }
 
-    pub fn remove_room(&self, _name: String) -> Result<(), String> {
+    pub fn remove_room(&self, _name: String) -> Result<(), Error> {
         todo!()
     }
 
@@ -34,12 +34,37 @@ impl House {
     }
 }
 
+#[derive(Debug, Clone, PartialEq)]
+pub enum Error {
+    AlreadyExist,
+    NotExist,
+    NotUnique,
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+            Error::AlreadyExist => write!(f, "House already contains a room with the name"),
+            Error::NotExist => write!(f, "Room doesn't exist"),
+            Error::NotUnique => write!(f, "Room name not unique"),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    fn test_house() -> House {
+    fn test_empty_house() -> House {
         House::new(String::from("MyHouse"))
+    }
+
+    fn test_house() -> House {
+        let house = test_empty_house();
+        let _res = house.add_room(test_room());
+        house
     }
 
     fn test_room() -> Room {
@@ -48,30 +73,35 @@ mod tests {
 
     #[test]
     fn create_house() {
-        let house = test_house();
-        assert_eq!(house.name, "MyHouse");
+        let house = test_empty_house();
+        assert_eq!(house.name, test_empty_house().name);
         assert_eq!(house.list_rooms(), Vec::<String>::new());
     }
 
     #[test]
-    fn add_remove_room() {
-        let house = test_house();
-        let room = test_room();
-        let _res = house.add_room(room);
+    fn add_room() {
+        let house = test_empty_house();
+        assert_eq!(house.add_room(test_room()), Ok(()));
+        assert_eq!(house.add_room(test_room()), Err(Error::AlreadyExist));
         assert_eq!(house.list_rooms(), vec![test_room().name]);
-        let _res2 = house.remove_room(String::from(test_room().name));
+
+        let house2 = House::new(String::from("MyHouse2"));
+        assert_eq!(house2.add_room(test_room()), Err(Error::NotUnique));
+    }
+
+    #[test]
+    fn remove_room() {
+        let house = test_house();
+        assert_eq!(house.list_rooms(), vec![test_room().name]);
+        assert_eq!(house.remove_room(String::from(test_room().name)), Ok(()));
+        assert_eq!(house.remove_room(test_room().name), Err(Error::NotExist));
         assert_eq!(house.list_rooms(), Vec::<String>::new());
     }
 
     #[test]
     fn get_room() {
         let house = test_house();
-        let room = test_room();
-        let _res = house.add_room(room);
-        let room2 = match house.get_room(test_room().name) {
-            Ok(u) => u,
-            Err(_e) => unreachable!(),
-        };
-        assert_eq!(room2.name, test_room().name);
+        assert_eq!(house.get_room(test_room().name).is_some(), true);
+        assert_eq!(house.get_room(String::from("MyRoom2")).is_none(), true);
     }
 }
--- src/room.rs
+++ src/room.rs
@@ -13,15 +13,15 @@ impl Room {
         }
     }
 
-    pub fn add_device(&self, _device_type: DeviceType) -> Result<(), String> {
+    pub fn add_device(&self, _device_type: DeviceType) -> Result<(), Error> {
         todo!()
     }
 
-    pub fn get_device(&self, _device_name: String) -> Result<DeviceType, String> {
+    pub fn get_device(&self, _device_name: String) -> Option<DeviceType> {
         todo!()
     }
 
-    pub fn remove_device(&self, _device_name: String) -> Result<(), String> {
+    pub fn remove_device(&self, _device_name: String) -> Result<(), Error> {
         todo!()
     }
 
@@ -29,6 +29,25 @@ impl Room {
         todo!()
     }
 }
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Error {
+    AlreadyExist,
+    NameUsed,
+    NotExist,
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+            Error::AlreadyExist => write!(f, "Room already contains a device with the name"),
+            Error::NameUsed => write!(f, "Room already contains another device with the name"),
+            Error::NotExist => write!(f, "Device doesn't exist"),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
 
 #[cfg(test)]
 mod tests {
@@ -69,14 +88,59 @@ mod tests {
     }
 
     #[test]
-    fn add_remove_device() {
-        let room = test_room();
+    fn add_device() {
+        let room = test_empty_room();
+        assert_eq!(
+            room.add_device(DeviceType::SmartSocket(test_smartsocket())),
+            Ok(())
+        );
+        assert_eq!(
+            room.add_device(DeviceType::Thermometer(test_thermometer())),
+            Ok(())
+        );
+        assert_eq!(
+            room.add_device(DeviceType::SmartSocket(test_smartsocket())),
+            Err(Error::AlreadyExist)
+        );
+        assert_eq!(
+            room.add_device(DeviceType::Thermometer(test_thermometer())),
+            Err(Error::AlreadyExist)
+        );
+        assert_eq!(
+            room.add_device(DeviceType::SmartSocket(SmartSocket {
+                name: String::from("Thermometer"),
+                description: String::from("A type of socket"),
+            })),
+            Err(Error::NameUsed)
+        );
+        assert_eq!(
+            room.add_device(DeviceType::Thermometer(Thermometer {
+                name: String::from("SmartSocket"),
+                description: String::from("A type of thermometer"),
+            })),
+            Err(Error::NameUsed)
+        );
         assert_eq!(
             room.list_devices(),
             vec![test_smartsocket().name, test_thermometer().name]
         );
-        let _res1 = room.remove_device(String::from(test_smartsocket().name));
-        let _res2 = room.remove_device(String::from(test_thermometer().name));
+    }
+
+    #[test]
+    fn remove_device() {
+        let room = test_room();
+        assert_eq!(
+            room.remove_device(String::from(test_smartsocket().name)),
+            Ok(())
+        );
+        assert_eq!(
+            room.remove_device(String::from(test_thermometer().name)),
+            Ok(())
+        );
+        assert_eq!(
+            room.remove_device(String::from(test_thermometer().name)),
+            Err(Error::NotExist)
+        );
         assert_eq!(room.list_devices(), Vec::<String>::new());
     }
 
@@ -84,22 +148,23 @@ mod tests {
     fn get_device() {
         let room = test_room();
         match room.get_device(test_smartsocket().name) {
-            Ok(u) => match u {
+            Some(u) => match u {
                 DeviceType::SmartSocket(device) => {
                     assert_eq!(test_smartsocket().name, device.name);
                 }
                 DeviceType::Thermometer(_device) => unreachable!(),
             },
-            Err(_e) => unreachable!(),
+            None => unreachable!(),
         };
         match room.get_device(test_thermometer().name) {
-            Ok(u) => match u {
+            Some(u) => match u {
                 DeviceType::SmartSocket(_device) => unreachable!(),
                 DeviceType::Thermometer(device) => {
                     assert_eq!(test_thermometer().name, device.name);
                 }
             },
-            Err(_e) => unreachable!(),
+            None => unreachable!(),
         };
+        assert_eq!(room.get_device(String::from("MyDevice")).is_none(), true);
     }
 }
